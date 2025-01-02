ddd2d3d39a80a8ac2b0052ad00a2e40778a2a617
Ying Tong <yingtong0106@gmail.com>
2025-01-02 20:38:47 +0800

    Added starter code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
-//  CCP6114 Programming Fundamentals
+//  ************************************************************************************************
+//  Program: main.cpp
+//  Course: CCP6114 Programming Fundamentals
+//  Lecture Class: TC1L
+//  Tutorial Class: TT1L
+//  Trimester: 2430
+//  Member_1: 242UC244NK | CHOW YING TONG | chow.ying.tong@student.mmu.edu.my | 016-5767692
+//  Member_2: 242UC244NK | LAW CHIN XUAN | law.chin.xuan@student.mmu.edu.my | 011-10988658
+//  ************************************************************************************************
+//  Task Distribution
+//  Member_1:
+//  Member_2: 
+//  ************************************************************************************************
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    return 0;
+}
