8456130285c6efedca3e7a542ba336f1c5c7db83
Ying Tong <yingtong0106@gmail.com>
2025-01-02 22:32:39 +0800

    Added more starter code, created input file and .gitignore

--- main.cpp
+++ main.cpp
@@ -15,8 +15,55 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <sstream>
 using namespace std;
 
+ifstream inputFile;
+ofstream outputFile;
+
+void readFileInput();
+void createTable();
+
+vector<string> tokenize(const string &line) {
+    vector<string> tokens;
+    stringstream stream(line);
+    string token;
+
+    while (stream >> token) {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
+
 int main() {
+    readFileInput();
     return 0;
+}
+
+void readFileInput() {
+    inputFile.open("fileInput1.mdb");
+
+    string line;
+    while (getline(inputFile, line)) {
+        cout << "Currently reading: " << line << endl;
+        
+        vector<string> tokens = tokenize(line);
+
+        if (!tokens.empty() && tokens[0] == "CREATE") {
+            if (tokens[1] == "TABLE") {
+                cout << "This is a table creation operation." << endl;
+            }
+            else if (tokens[1].length() >= 4 && tokens[1].substr(tokens[1].length() - 5, 5) == ".txt;") {
+                cout << "Creating output file..." << endl;
+            }
+        }
+    }
+
+    inputFile.close();
+}
+
+void createTable() {
+
 }
