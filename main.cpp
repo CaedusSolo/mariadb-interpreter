3624c9a3afb6bcc6891102de0be87e5a24708b54
Ying Tong <yingtong0106@gmail.com>
2025-01-05 21:40:09 +0800

    Attempt at completing the tokenization function

--- main.cpp
+++ main.cpp
@@ -22,20 +22,45 @@ using namespace std;
 ifstream inputFile;
 ofstream outputFile;
 
+vector<string> tokenize(const string&);
 void readFileInput();
-void createTable();
+void createTable(const vector<string> &);
 
 vector<string> tokenize(const string &line) {
     vector<string> tokens;
     stringstream stream(line);
     string token;
 
-    while (stream >> token) {
-        tokens.push_back(token);
+    while (getline(stream, token, ' ')) {
+        if (!token.empty()) {
+            size_t pos = token.find('(');
+            if (pos != string::npos) {
+                if (pos > 0) {
+                    tokens.push_back(token.substr(0, pos));
+                }
+                tokens.push_back("(");
+                if (pos + 1 < token.length()) {
+                    tokens.push_back(token.substr(pos + 1));
+                }
+            } else {
+                pos = token.find(')');
+                if (pos != string::npos) {
+                    if (pos > 0) {
+                        tokens.push_back(token.substr(0, pos));
+                    }
+                    tokens.push_back(")");
+                    if (pos + 1 < token.length()) {
+                        tokens.push_back(token.substr(pos + 1));
+                    }
+                } else {
+                    tokens.push_back(token);
+                }
+            }
+        }
     }
-
     return tokens;
 }
+
 
 int main() {
     readFileInput();
@@ -43,27 +68,37 @@ int main() {
 }
 
 void readFileInput() {
-    inputFile.open("fileInput1.mdb");
+    inputFile.open("fileInput1.mdb"); //  open input file
 
-    string line;
+    string line;  //  initialize variable to store content of each line
+
     while (getline(inputFile, line)) {
-        cout << "Currently reading: " << line << endl;
-        
+        if (line.empty()) continue;  //  skip tokenization for empty lines
         vector<string> tokens = tokenize(line);
+
+        for (int i = 0; i < tokens.size(); i++) {
+            cout << tokens[i] << endl;
+        }
 
         if (!tokens.empty() && tokens[0] == "CREATE") {
             if (tokens[1] == "TABLE") {
                 cout << "This is a table creation operation." << endl;
+                createTable(tokens);
             }
             else if (tokens[1].length() >= 4 && tokens[1].substr(tokens[1].length() - 5, 5) == ".txt;") {
                 cout << "Creating output file..." << endl;
             }
         }
+
+        else if (!tokens.empty() && tokens[0] == "INSERT" && tokens[1] == "INTO") {
+            createTable(tokens);
+        }
+
     }
 
     inputFile.close();
 }
 
-void createTable() {
-
+void createTable(const vector<string> &tokens) {
+    string tableName = tokens[2];
 }
